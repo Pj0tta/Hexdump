fa088f7cb397f54d65f5e24f7648f326b0ad6a3e
Petr Habel <petrhabel04@gmail.com>
2022-02-06 17:25:41 +0100

    Přidejte soubory projektu.

---
+++ Hexdump.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    //FILE* ascii = fopen("vystup-txt-bin.txt", "w");
+    FILE* ascii = fopen("vystup-txt-bin.txt", "wb");
+
+    int x = 0x1A2B3C4D;
+    fprintf(ascii, "%d\n", x);
+    fwrite(&x, sizeof(int), 1, ascii);
+
+    fclose(ascii);
+
+}
+
